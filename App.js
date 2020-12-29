5e8226d71d5aaf7935bc999f1ae0abc6f936285a
Gangyong Hyeong <hyeonggang@naver.com>
2020-12-29 11:00:26 +0900

    #2.0 Fokin-Weather ~#1.4

--- App.js
+++ App.js
@@ -1,7 +1,39 @@
 import { StatusBar } from "expo-status-bar";
+import { Alert } from "react-native";
 import React from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "3f77e9643364891f97fc49111a712b30";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
