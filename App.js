dbd369b7f608d5d7890da5a88640e19b37285d2d
Gangyong Hyeong <hyeonggang@naver.com>
2020-12-29 11:23:53 +0900

    #3.0 Fokin-Weather ~#2.0

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { Alert } from "react-native";
 import React from "react";
-import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Loading from "./Loading";
+import Weather from "./Weather";
 
 const API_KEY = "3f77e9643364891f97fc49111a712b30";
 
@@ -13,9 +14,9 @@ export default class extends React.Component {
   };
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
+    this.setState({ isLoading: false, temp: data.main.temp });
   };
   getLocation = async () => {
     try {
@@ -24,6 +25,8 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
+      // 일시적으로 temp 값이 없는 경우가 있어 Nan 뜨는 경우가 있음. isLoading을 여기서 false로 바꾸는건
+      // 아닌 듯 싶다.
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
@@ -33,7 +36,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
